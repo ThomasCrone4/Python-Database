3419125beb0263cb83bfcb73ab9d7079a79ad8d6
ThomasCrone4 <thomascrone04@gmail.com>
2024-12-22 08:06:21 +0000

    Adding and removing record from database

--- main.py
+++ main.py
@@ -21,20 +21,43 @@ def update_user_email(user_id, new_email):
     else:
         print(f"User with id {user_id} not found")
 
+def remove_user(user_id):
+    try:
+        user = session.query(User).filter(User.user_id == user_id).first()
+        if user:
+            session.delete(user)
+            session.commit()
+            print(f"User {user_id} has been removed")
+        else:
+            print(f"User with id {user_id} not found")
+    except Exception as e:
+        print(f"Error removing user {user_id}: {e}")
+        session.rollback()
+
 # Update the email of user with id 1
 update_user_email(1, 'email@123.com')
 
 # Add a new user
-new_user = User(username='john_doe', email= "john@example.com", password='securepassword')  
+new_user = User(username='stev_doe', email= "stve@example.com", password='securepassword')  
 session.add(new_user)
-new_user_1 = User(username='bob_doe', email='bob@example.com', password='securepassword')
+try:
+    session.commit()
+    print("New user added successfully")
+except IntegrityError as e:
+    session.rollback()
+    print("Error: {e.orig}. A user with this username or email already exists")
+
+new_user_1 = User(username='tm_doe', email='tm@example.com', password='securepassword')
 session.add(new_user_1)
 try:
     session.commit()
     print("New user added successfully")
-except IntegrityError:
+except IntegrityError as e:
     session.rollback()
-    print("Error: A user with this username or email already exists")
+    print("Error: {e.orig}. A user with this username or email already exists")
+
+remove_user(5)
+remove_user(6)
 
 
 # Verify the user was added
